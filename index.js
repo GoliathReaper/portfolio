93d57506635abb3dd7a4232ba3e47876a7fd8b4a
Yogesh <77969919+GoliathReaper@users.noreply.github.com>
2024-07-13 13:13:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+
+document.querySelector('.glass-morphism-container-hover-effect').onmousemove = (e) => {
+  const x = e.pageX - e.target.offsetLeft
+  const y = e.pageY - e.target.offsetTop
+  e.target.style.setProperty('--x', `${ x }px`)
+  e.target.style.setProperty('--y', `${ y }px`)
+ 
+}
